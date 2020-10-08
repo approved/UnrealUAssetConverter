62a74a88d2cd56bfa10e0eeca3f2aab414888cfd
Nick Kennedy <nickkennedy334@gmail.com>
2020-10-08 00:26:04 -0400

    Included Example Project for Reading UAsset data

---
+++ Examples/ReadUAssetInfo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using UnrealUAssetConverter;
+using UnrealUAssetConverter.Unreal;
+
+namespace ReadUAssetInfo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                if (File.Exists(args[0]))
+                {
+                    using (UAssetConverter uaConverter = new UAssetConverter(args[0]))
+                    {
+                        FPackageFileSummary summ = uaConverter.GetSummary();
+                        Console.WriteLine($"Package Size: {summ.TotalSize} bytes");
+                        Console.WriteLine($"Package Flags: {summ.PackageFlags}");
+                        Console.WriteLine($"{summ.ImportCount} imported objects");
+                        Console.WriteLine($"{summ.ExportCount} exported objects");
+                    }
+                }
+            }
+        }
+    }
+}
