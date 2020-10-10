9c1896d571e16af0fea917b19c0ed4fb93214ab1
Nick Kennedy <nickkennedy334@gmail.com>
2020-10-10 19:10:41 -0400

    Fixed FObjectImport from reading the data in the incorrect order

--- UConvertPlugin/Unreal/FObjectImport.cs
+++ UConvertPlugin/Unreal/FObjectImport.cs
@@ -8,12 +8,16 @@ namespace UConvertPlugin.Unreal
         public FName ClassPackage;
         public FName ClassName;
 
-        public FObjectImport(IAssetConverter converter) : base(converter)
+        public FObjectImport(IAssetConverter converter)
         {
             using (BinaryReader br = new BinaryReader(converter.GetAssetStream(), Encoding.UTF8, true))
             {
                 this.ClassPackage = new FName(converter.GetNameMap(), converter.GetAssetStream());
                 this.ClassName = new FName(converter.GetNameMap(), converter.GetAssetStream());
+
+                FObjectResource resource = new FObjectResource(converter);
+                this.Index = resource.Index;
+                this.ObjectName = resource.ObjectName;
             }
         }
     }
